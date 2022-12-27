f3667af132dae9adffef95297c7c80aee14a94a4
samintisar <samintisaruni@gmail.com>
2022-12-26 23:14:02 -0800

    set up the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import './App.css'
+import ChatFeed from './components/ChatFeed.jsx'
+
+import { ChatEngine } from 'react-chat-engine'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ChatEngine
+        height = "100vh"
+        projectID = "b1b419eb-8a46-45ea-adac-48a6b427f107"
+        userName = "john"
+        userSecret = "qwerty"
+        renderChatFeed = { (chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        onNewMessage = { () => new Audio("https://chat-engine-assets.s3.amazonaws.com/click.mp3").play() }
+      />
     </div>
-  );
+  )
 }
 
 export default App;
